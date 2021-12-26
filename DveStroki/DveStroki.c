0673bb56f2aa7006268818682a102328f766e174
Pavel <51826654+1commando1@users.noreply.github.com>
2021-12-26 12:07:16 +0300

    Добавьте файлы проекта.

---
+++ DveStroki/DveStroki.c
@@ -0,0 +1,22 @@
+﻿#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	char s[50], s1[50], s2[50];
+	int i;
+	s[0] = 0;
+	printf("Введите первую строку: ");
+	gets(s1);
+	printf("Введите вторую строку: ");
+	gets(s2);
+	strcpy(s, s1);
+	strcat(s, s2);	
+	printf("Результат: ");
+	puts(s);
+	strcpy(s, s2);
+	strcat(s, s1);	
+	
+	return 0;
+}
